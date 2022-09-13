fba523c3a83ae98d8b37ad13bd3088f4d72151e9
Victoria <veerod0804@yahoo.com>
2022-09-12 20:23:38 -0500

    Complete unit testing studio for lesson 5

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,63 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void openAndClosedBracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void stringInsideBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[hello]"));
+    }
+    @Test
+    public void nestedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void onlyOpenBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void testBracketsInsideOutFramingWordReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Oh ]hello[ there"));
+    }
+    @Test
+    public void bracketInWrongOrderReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void twoStatements() {
+        String hello = "[hello]";
+        String world = "Wo][rld";
+
+        assertEquals(!BalancedBrackets.hasBalancedBrackets(hello), BalancedBrackets.hasBalancedBrackets(world));
+    }
+
+    @Test
+    public void onlySingleBracketsInterlaced() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void onlySingleBracketsBeforeOtherChars() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Hello"));
+    }
+
+    @Test
+    public void twoSetsOfBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode[]"));
+    }
+
+    @Test
+    public void leftBraceOnlyReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void stringWithNoBraces() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
 }
